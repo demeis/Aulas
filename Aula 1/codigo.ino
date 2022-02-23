08b868a679a7bc884487380f4c53ef1d923d0fe8
Gustavo Demeis <79205993+demeis@users.noreply.github.com>
2022-02-23 12:50:06 -0300

    Create codigo.ino

---
+++ Aula 1/codigo.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  delay(200);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  delay(200);
+}
