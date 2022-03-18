ec572da71bbba90a8f520648bac786b707143dea
Gustavo Demeis <79205993+demeis@users.noreply.github.com>
2022-03-18 17:09:47 -0300

    Create codigo2.ino

---
+++ Exercícios/codigo2.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000); 
+  digitalWrite(2, LOW);
+  digitalWrite(4, HIGH);
+  delay(1000); 
+  digitalWrite(4, LOW);
+}
